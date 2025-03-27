7b08e577be1756efcdfd0857e4fb24e3260ea66f
Ivan Ordinarov <tojo@inbox.ru>
2025-03-27 22:38:45 +0300

    third part of the errors has been fixed

--- src/datastorage/Storage.java
+++ src/datastorage/Storage.java
@@ -3,10 +3,13 @@ package datastorage;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
 
 public class Storage {
-    String randomWord;
+    private String randomWord;
     static Random randomizer = new Random();
     public List<String> words = new ArrayList<>();
 
