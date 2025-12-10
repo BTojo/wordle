6322f1afde1d9a4da854214193be3d099bb7aac4
Ivan Ordinarov <tojo@inbox.ru>
2025-12-10 22:31:33 +0300

    working version 44.1 BD

--- src/main/java/ru/wordle/infrastructure/configuration/LiquibaseConfig.java
+++ src/main/java/ru/wordle/infrastructure/configuration/LiquibaseConfig.java
@@ -1,6 +1,7 @@
 package ru.wordle.infrastructure.configuration;
 
 import liquibase.integration.spring.SpringLiquibase;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -9,13 +10,23 @@ import javax.sql.DataSource;
 @Configuration
 public class LiquibaseConfig {
 
+    @Value("${liqui.enabled:true}")
+    private boolean liquibaseEnabled;
+
+    // Читаем имя схемы из application.properties
+    @Value("${liqui.default-schema:wordle}")
+    private String defaultSchema;
+
     @Bean
     public SpringLiquibase liquibase(DataSource dataSource) {
         SpringLiquibase liquibase = new SpringLiquibase();
         liquibase.setDataSource(dataSource);
 
         liquibase.setChangeLog("classpath:db/changelog/db.changelog-master.xml");
-        liquibase.setDefaultSchema("wordle");
+
+        liquibase.setDefaultSchema(defaultSchema);
+
+        liquibase.setShouldRun(liquibaseEnabled);
 
         return liquibase;
     }
