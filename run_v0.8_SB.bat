6bc05e907414761204cd56f85ba83fdc7a9d140e
Ivan Ordinarov <tojo@inbox.ru>
2026-03-25 21:20:04 +0300

    working version 55.2_Spring Boot!_BD

--- run_v0.8_SB.bat
+++ run_v0.8_SB.bat
@@ -1,14 +1,14 @@
 @echo off
-echo Building project...
+echo [%date% %time%] Starting build...
 call mvn clean package
 if %errorlevel% neq 0 (
-    echo Build failed!
+    echo [%date% %time%] Build failed!
     pause
     exit /b %errorlevel%
 )
 
 echo.
-echo Starting application...
+echo [%date% %time%] Build successful! Starting application...
 call mvn spring-boot:run
 
 pause
