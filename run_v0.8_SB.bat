9dfe2ebb9a7ac23bd64f66da575f41887244dfe9
Ivan Ordinarov <tojo@inbox.ru>
2026-03-25 21:12:13 +0300

    working version 55.1_Spring Boot!_BD

--- run_v0.8_SB.bat
+++ run_v0.8_SB.bat
@@ -1,2 +1,14 @@
-mvn clean package
-mvn spring-boot:run
+@echo off
+echo Building project...
+call mvn clean package
+if %errorlevel% neq 0 (
+    echo Build failed!
+    pause
+    exit /b %errorlevel%
+)
+
+echo.
+echo Starting application...
+call mvn spring-boot:run
+
+pause
