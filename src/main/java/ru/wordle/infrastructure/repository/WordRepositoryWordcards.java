b934e8a737e9153f3129f54cf4869c4c46212469
Ivan Ordinarov <tojo@inbox.ru>
2026-07-01 12:24:29 +0300

    working version 61_Spring Boot + UiController!_BD

--- src/main/java/ru/wordle/infrastructure/repository/WordRepositoryWordcards.java
+++ src/main/java/ru/wordle/infrastructure/repository/WordRepositoryWordcards.java
@@ -1,23 +1,25 @@
 package ru.wordle.infrastructure.repository;
 
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Profile;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Repository;
-import org.springframework.web.client.RestTemplate;
 import ru.wordle.domain.repository.WordRepository;
+import ru.wordle.infrastructure.wordcards.WordcardsClient;
 import ru.wordle.infrastructure.wordcards.WordcardsRequest;
 import ru.wordle.infrastructure.wordcards.WordcardsResponse;
 
-import java.util.*;
+import java.util.List;
+import java.util.Locale;
+import java.util.Random;
 
 @Slf4j
 @Repository
 @Profile("wordcards")
+@RequiredArgsConstructor
 public class WordRepositoryWordcards implements WordRepository {
 
-    private static final String API_URL = "https://wordcards.ru/api/wordle";
-    private final RestTemplate restTemplate = new RestTemplate();
+    private final WordcardsClient wordcardsClient;
     private final Random random = new Random();
 
     @Override
@@ -27,13 +29,8 @@ public class WordRepositoryWordcards implements WordRepository {
                 "ru", "5", "", "", "", false
         );
 
-        ResponseEntity<WordcardsResponse> response = restTemplate.postForEntity(
-                API_URL, request, WordcardsResponse.class
-        );
-
-        List<WordcardsResponse.WordItem> words = response.getBody() != null
-                ? response.getBody().getPayload()
-                : null;
+        WordcardsResponse response = wordcardsClient.getWords(request);
+        List<WordcardsResponse.WordItem> words = response.getPayload();
 
         if (words == null || words.isEmpty()) {
             throw new RuntimeException("No words returned from wordcards.ru");
@@ -51,13 +48,8 @@ public class WordRepositoryWordcards implements WordRepository {
                 "ru", "5", "", "", word, false
         );
 
-        ResponseEntity<WordcardsResponse> response = restTemplate.postForEntity(
-                API_URL, request, WordcardsResponse.class
-        );
-
-        List<WordcardsResponse.WordItem> words = response.getBody() != null
-                ? response.getBody().getPayload()
-                : null;
+        WordcardsResponse response = wordcardsClient.getWords(request);
+        List<WordcardsResponse.WordItem> words = response.getPayload();
 
         boolean exists = words != null && !words.isEmpty();
         log.info("Word '{}' exists: {}", word, exists);
---
+++ src/main/java/ru/wordle/infrastructure/wordcards/WordcardsClient.java
@@ -0,0 +1,16 @@
+package ru.wordle.infrastructure.wordcards;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+@FeignClient(
+        name = "wordcards",
+        url = "${feign.client.config.wordcards.url}",
+        path = "${feign.client.config.wordcards.api-base-url}"
+)
+public interface WordcardsClient {
+
+    @PostMapping("/wordle")
+    WordcardsResponse getWords(@RequestBody WordcardsRequest request);
+}
